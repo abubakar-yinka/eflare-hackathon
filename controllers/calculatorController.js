375b944432561379805ae35756446ae2ae76312d
abubakar-yinka <abubakaryinka75@gmail.com>
2021-09-05 22:17:34 +0100

    Autofill endpoint created and excel datasheet read to generate a random set of data

--- controllers/calculatorController.js
+++ controllers/calculatorController.js
@@ -1,21 +1,48 @@
 const mongoose = require('mongoose');
-const Store = mongoose.model('Store');
+// const Store = mongoose.model('Store');
+const xlsx = require('xlsx');
 
-exports.calculator = async (req, res) => {
-    try {
-        const temperature = req.body.temperature;
-        const water_injection = req.body.water_injection;
-        const moisture = req.body.moisture;
-        const pressure_levels = req.body.pressure_levels;
-        const gas_flow_rate = req.body.gas_flow_rate;
-        const gas_injection = req.body.gas_injection;
-        const no_of_wells_on_site = req.body.no_of_wells_on_site;
-        const water_rate = req.body.water_rate;
+const workbook = xlsx.readFile(__dirname + '/data/Oil-well.xlsx');
 
-        const store = new Store(req.body);
-        await store.save()
+const ExcelDateToJSDate = (serial) => {
+    var utc_days  = Math.floor(serial - 25569);
+    var utc_value = utc_days * 86400;                                        
+    var date_info = new Date(utc_value * 1000);
+ 
+    var fractional_day = serial - Math.floor(serial) + 0.0000001;
+ 
+    var total_seconds = Math.floor(86400 * fractional_day);
+ 
+    var seconds = total_seconds % 60;
+ 
+    total_seconds -= seconds;
+ 
+    var hours = Math.floor(total_seconds / (60 * 60));
+    var minutes = Math.floor(total_seconds / 60) % 60;
+ 
+    return new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds);
+ }
 
-    } catch(err) {
-        
+let worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+
+const get_oil_well_data = () => {
+    const well_data = [] 
+    for (let index = 4; index < 2943; index++) {
+        let oil_well_data = {}
+        oil_well_data.date = ExcelDateToJSDate(worksheet[`A${index}`].v);
+        oil_well_data.oil_volume = worksheet[`B${index}`].v;
+        oil_well_data.vol_of_liquid = worksheet[`C${index}`].v;
+        oil_well_data.gas_volume = worksheet[`D${index}`].v;
+        oil_well_data.water_volume = worksheet[`E${index}`].v;
+        oil_well_data.water_cut = worksheet[`F${index}`].v;
+        oil_well_data.working_hours = worksheet[`G${index}`].v;
+        oil_well_data.dynamic_level = worksheet[`H${index}`].v;
+        oil_well_data.reservoir_pressure = worksheet[`I${index}`].v;
+        well_data.push(oil_well_data)
     }
+    return well_data
 }
+
+
+exports.autofill_data = get_oil_well_data();
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const calculatorController = require('../controllers/calculatorController')
 
-// router.post('/calculate', calculatorController)
+const random_well_parameters = calculatorController.autofill_data[Math.floor(Math.random() * calculatorController.autofill_data.length)];
 
-module.exports = router
+console.log(random_well_parameters);
+router.get('/autofill', (req, res) => {
+    res.json(random_well_parameters)
+})
+
+module.exports = router;
