e6f83773458213075bc2522921addf0b426543b6
abubakar-yinka <abubakaryinka75@gmail.com>
2021-09-06 01:21:14 +0100

    Dist folder added

---
+++ client/.nuxt/dist/server/pages/map/index.js
@@ -0,0 +1,68 @@
+exports.ids = [8];
+exports.modules = {
+
+/***/ 52:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/map/index.vue?vue&type=template&id=501ade26&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('GmapMap',{staticStyle:{"width":"500px","height":"300px"},attrs:{"center":{lat:10, lng:10},"zoom":7,"map-type-id":"terrain"}})],1)}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./pages/map/index.vue?vue&type=template&id=501ade26&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/map/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var mapvue_type_script_lang_js_ = ({});
+// CONCATENATED MODULE: ./pages/map/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pages_mapvue_type_script_lang_js_ = (mapvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/map/index.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_mapvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "6511de45"
+  
+)
+
+/* harmony default export */ var map = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=index.js.map
